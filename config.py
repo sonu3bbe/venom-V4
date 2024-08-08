851a33c37520eca31d6cac7e7ae20ffb696415d8
sonu3bbe <130968948+sonu3bbe@users.noreply.github.com>
2024-08-09 00:15:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7118996438:AAG2KAw0xynp1kv4Q5UUwdj0OLXOo4cVW0Y")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Sunillgg")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6898426626")
 
 
 
